afea9337b46d77fff73510ecbabbb399054fb320
Frontend JavaScript <frontendjavascript@C3-12.local>
2023-03-28 20:23:43 +0300

    curs2

--- curs1/js/Carte.js
+++ curs1/js/Carte.js
@@ -1,32 +1,47 @@
 class Carte{
-    constructor(titlu,autor,editura,an_aparitie,pret,nr_pagini)  {
+    static reducere = 10;
+    mypret;
+    constructor(titlu,autor,editura,an_aparitie,nr_pagini)  {
         this.titlu=titlu;
         this.autor = autor;
         this.editura = editura;
         this.an_aparitie = an_aparitie;
-        this.pret = pret;
         this.nr_pagini=nr_pagini
     } 
+    get pret(){
+        console.log('sunt in getter');
+        return this.mypret;
+    }
+    set pret(pretNou){
+        console.log('sunt in setter');
+        if(pretNou<=0){
+            throw 'Pretul nu poate fi actualizat'
+        }
+        else{
+            this.mypret=pretNou;
+        }
+       
+    }
+    info(){
+        console.log(`Titlul este ${this.titlu}, autor ${this.autor},editura ${this.editura}`);
+    }
     obtine_titlu(){
         console.log(`${this.titlu}`)
     }
     obtine_autor(){
         console.log(`${this.autor}`)
     }
-    modifica_pret(pretNou){
-        if(pretNou < 100 && pretNou > this.pret){
-            this.pret = pretNou;
-        }
-    }
-    reducere(procent){
-        this.pret = this.pret - (procent/100* this.pret)
-        console.log(`Pretul cu reducere este ${this.pret}`)
-    }
+    // modificapret(pretNou){
+    //     if(pretNou < 100 && pretNou > this.pret){
+    //         this.pret = pretNou;
+    //     }
+    // }
+    // reducere(){
+    //     this.pret = this.pret - (Carte.reducere* this.pret) / 100
+    //     console.log(`Pretul cu reducere este ${this.pret}`)
+    // }
 
 }
 
-const carte = new Carte('Poezii','M.Eminescu','Humanites',2010,50,120);
-carte.modifica_pret(75);
-console.log(carte)
-carte.reducere(10)
-console.log()
+
+
